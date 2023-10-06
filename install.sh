c33790d1661aeffd45a216ea2395f63e44500ca9
Wildan Pratama <wildan-pratama@outlook.co.id>
2023-10-06 10:48:44 +0700

    add more wallpaper

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ if [ ! -d "$DIR" ]; then
     echo "Directory $DIR does not exist. Creating it now."
     mkdir -p $DIR
 fi
-cp -r backgrounds $HOME/.local/share 
+cp -r backgrounds $HOME/.local/share/ 
